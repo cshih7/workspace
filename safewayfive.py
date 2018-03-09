a0cb8e96a2d5813ea6600edc76230f33e05a7f8f
C S <Cindy@DN51s4on.SUNet>
2018-03-09 00:35:24 -0800

    updated to most recent file

--- safewayfive.py
+++ safewayfive.py
@@ -3,43 +3,123 @@
 #What about JavaScript enabled sites? http://stanford.edu/~mgorkove/cgi-bin/rpython_tutorials/Scraping_a_Webpage_Rendered_by_Javascript_Using_Python.php
 
 #2/5/18
-#---------#
-import code 
+# #-----SAFEWAY PORTION----#
+# import code 
 
-#find length of HTML on a page
-from mathematicians import simple_get
-#raw_html = simple_get('http://plan.safeway.com/Circular/Menlo-Park-525-El-Camino-Real/CCBC74236/Other/5')
-#print len(raw_html)
+# #find length of HTML on a page
+# from mathematicians import simple_get
+# raw_html = simple_get('http://plan.safeway.com/Circular/Menlo-Park-525-El-Camino-Real/CCBC74236/Other/5')
+# # print len(raw_html) 
+# # the last number at the end of this above Safeway link refers to the # week of the month it is (i.e., 3=3rd flyer of the month)
 
-"""
-raw_html = simple_get('http://plan.safeway.com/Circular/Menlo-Park-525-El-Camino-Real/CCBC74236/Other/5')
-#(following is optional code if i want to download into a file on my computer)
+# raw_html = simple_get('http://plan.safeway.com/Circular/Menlo-Park-525-El-Camino-Real/CCBC74236/Other/3')
+# # (following is optional code if i want to download into a file on my computer)
 # with open('safeway5.html', "w") as html_file:
 # 	html_file.write(raw_html)
 # html_file.close()
 
-#READING SAFEWAY'S FRIDAY $5 FLYER
+# #READING SAFEWAY'S FRIDAY $5 FLYER
+
+# from bs4 import BeautifulSoup
+# raw_html = open('safeway5.html').read()
+# html = BeautifulSoup(raw_html, 'html.parser')
+# with open('safewaydeals.txt', 'w') as outfile:
+# 	outfile.write('Safeway Friday $5 Deals' + '\n' + '\n')
+# 	for p in html.select('p'):
+# 		if p.has_attr('class'):
+# 			if p['class'] == ['itemPrice']:
+# 				outfile.write(p.text + '\n')
+# 			if p['class'] == ['itemTitle']:
+# 				outfile.write((p.text + '\n' + '\n').encode('utf8'))
+# outfile.close()
+
+#-----SPROUTS PORTION-----#
+
+# 2/7/18
+#Let's use Selenium to scrape from JavaScript-rendered page: 
+
+import time
+from utils.driver import get_driver
+driver = get_driver()
+
+driver.get('https://shop.sprouts.com/shop/flyer')
+
+def get_button_by_id(id):
+	try:
+		button = driver.find_element_by_id(id) 
+	except:
+		button = None
+
+	if not button:
+		time.sleep(2)
+		return get_button_by_id(id)
+
+	return button
+
+searchbutton = get_button_by_id('shopping-selector-search-cities')
+searchbutton.send_keys('94040')
+
+submitbutton = get_button_by_id('shopping-selector-update-home-store-157-instore')
+submitbutton.click()
+
+innerHTML = driver.execute_script("return document.body.innerHTML") #returns the innerHTML as a string
+# print len(innerHTML)
+
+
+with open('sproutsflyer.html', "w") as html_file:
+  html_file.write(innerHTML.encode('utf8'))
+  html_file.close()
+
+#html_file = file('sproutsflyer', 'r')
+#print html_file.read().decode('utf8')
 
 from bs4 import BeautifulSoup
-# raw_html = open('safeway5.html').read()
-html = BeautifulSoup(raw_html, 'html.parser')
-for p in html.select('p'):
-	# code.interact(local = locals())
-	if p.has_attr('class'):
-		if p['class'] == ['itemPrice']: <-- Watch out for "list" vs. str with [] or without []
-			print(p.text)
-		if p['class'] == ['itemTitle']:
-			#code.interact(local = locals())
-			print(p.text + '\n')
-"""
+sproutshtml = open('sproutsflyer.html').read()
+html = BeautifulSoup(sproutshtml, 'html.parser')
+with open('sproutsdeals.txt', 'w') as outfile:
+	outfile.write('Sprouts Front Page Deals' + '\n' + '\n')
+	for span in html.select('span'):
+		if span.has_attr('class'):
+			if span['class'] == ['cell-title-text']: #<-- Watch out for "list" vs. str with [] or without []
+				outfile.write('\n' + (span.text).encode('utf8') + '\n')		
+			if span['class'] == ['amount']:
+				if 'sale' in span.parent.parent['class']:
+					#code.interact(local = locals())
+					outfile.write((span.text).encode('utf8'))
+			if span['class'] == ['unit']:
+				if 'sale' in span.parent.parent['class']:	
+					outfile.write((span.text + '\n').encode('utf8'))
+outfile.close()
 
-#2/7/18
-#Let's use Selenium to scrape from JavaScript-rendered page: 
-#should pip install selenium first, may need to install a chrome web driver?
+# # -----WHOLE FOODS PORTION-----#
+# from mathematicians import simple_get
 
-from selenium import webdriver
+# raw_html = simple_get('https://www.wholefoodsmarket.com/sales-flyer/losaltos')
+# # (following is optional code if i want to download into a file on my computer)
+# with open('wholefoods.html', "w") as html_file:
+# 	html_file.write(raw_html)
+# html_file.close()
 
-browser = webdriver.Chrome() #replace with .Firefox(), or with the browser of your choice
-url = "https://shop.sprouts.com/shop/flyer"
-browser.get(url) #navigate to the page
-innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
+# from bs4 import BeautifulSoup
+# raw_html = open('wholefoods.html').read()
+# html = BeautifulSoup(raw_html, 'html.parser')
+# with open('wholefoodsdeals.txt', 'w') as outfile:
+# 	outfile.write('Whole Foods Deals of the Week' + '\n' + '\n')
+# 	for div in html.select('div'):
+# 		if div.has_attr('class'):
+# 			if 'views-field-field-flyer-brand-name' in div['class']: 
+# 				outfile.write((div.text).encode('utf8') + ' ')
+# 			if 'views-field-field-flyer-product-name' in div['class']:
+# 				outfile.write(div.text + '\n')
+# 			if div['class'] == ['sale_line']:
+# 				outfile.write(div.text + '\n')		
+# 			if div['class'] == ['reg_line']:
+# 				outfile.write(div.text + '\n')
+# outfile.close()
+
+# # ----END ---- #
+
+#now, read through the html and find the tag to spit out the sale items
+#then maybe compile safeway + sprouts in excels
+#create a list of dictionaries (item name, slug, price) 
+
--- selenium_test.py
+++ selenium_test.py
@@ -7,10 +7,25 @@ from utils.driver import get_driver
 
 driver = get_driver()
 
-driver.get('http://www.google.com/xhtml');
+driver.get('http://www.google.com/xhtml')
 time.sleep(5) # Let the user actually see something!
 search_box = driver.find_element_by_name('q')
 search_box.send_keys('ChromeDriver')
 search_box.submit()
 time.sleep(5) # Let the user actually see something!
 driver.quit()
+
+'''
+# to send login information to a website
+username = browser.find_element_by_id("username_id") #username form field
+password = browser.find_element_by_id("password_id") #password form field
+
+username.send_keys("my_username")
+password.send_keys("my_password")
+
+submitButton = browser.find_element_by_id("submit_button_id") 
+button.click() 
+
+#Retrieving the inner HTML
+browser.get("http://example.com/page.php") #navigate to page behind login
+innerHTML = browser.execute_script("return document.body.innerHTML") #returns the inner HTML as a string
